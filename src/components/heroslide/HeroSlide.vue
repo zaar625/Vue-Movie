99dcfae6738ed98b00312768d1b0b8163359f469
zaar625 <zaar625@naver.com>
2022-01-09 21:47:34 +0900

    Header-slide 이미지 구현

--- src/components/heroslide/HeroSlide.vue
+++ src/components/heroslide/HeroSlide.vue
@@ -2,19 +2,13 @@
   <div class="hero-slide">
     <Swiper
       class="a"
-      :slides-per-view="3"
+      :slides-per-view="1"
       :space-between="50"
       @swiper="onSwiper"
       @slideChange="onSlideChange"
     >
-      <swiper-slide v-slot="{ isActive }">
-        <div>slide1 {{ isActive ? 'active' : 'not active' }}</div>
-      </swiper-slide>
-      <swiper-slide v-slot="{ isActive }">
-        <div>slide12 {{ isActive ? 'active' : 'not active' }}</div>
-      </swiper-slide>
-      <swiper-slide v-slot="{ isActive }">
-        <div>slide13{{ isActive ? 'active' : 'not active' }}</div>
+      <swiper-slide v-for="(item, index) in movieItems">
+        <img :src="image(item.backdrop_path)" />
       </swiper-slide>
     </Swiper>
   </div>
@@ -23,11 +17,20 @@
 <script>
 import './hero-slide.scss';
 import { Swiper, SwiperSlide } from 'swiper/vue';
-// import SwiperCore, { Autoplay } from 'swiper';
-// SwiperCore.use([Autoplay]);
+import SwiperCore, { Autoplay } from 'swiper';
+import axios from 'axios';
+import { apiConfig } from '../../api/apiConfig';
+
+SwiperCore.use([Autoplay]);
 import 'swiper/swiper.min.css';
 
 export default {
+  data() {
+    return {
+      movieItems: [],
+      backgroundImages: '',
+    };
+  },
   components: {
     Swiper,
     SwiperSlide,
@@ -44,11 +47,29 @@ export default {
       onSlideChange,
     };
   },
+  created: function () {
+    const getMovie = async () => {
+      const { data } = await axios.get(`
+        https://api.themoviedb.org/3/discover/movie?api_key=af43ac72d70dd07b3747f0dc7b4a2680&language=ko&sort_by=popularity.desc&include_adult=false&page=1`);
+      console.log(data);
+      this.movieItems.push(...data.results.slice(1, 4));
+      console.log(this.movieItems);
+    };
+    getMovie();
+  },
+  methods: {
+    image(image) {
+      return apiConfig.originalImage(image);
+    },
+  },
 };
 </script>
 
 <style>
 .a {
   color: white;
+}
+.b {
+  height: 500px;
 }
 </style>
